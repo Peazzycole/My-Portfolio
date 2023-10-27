9286607d2cede5cb3ac3526cdb95d924a8ec20c4
Peace Oghenevwefe <Peazzycole@gmail.com>
2023-10-27 21:48:09 +0100

    made it faster

--- components/Transition.tsx
+++ components/Transition.tsx
@@ -5,7 +5,7 @@ const Transition = () => {
   return (
     <>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen "
         key={1}
         initial={{ x: 1050, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
@@ -15,7 +15,7 @@ const Transition = () => {
         exit={{ x: 1050, opacity: 0 }}
       />
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen "
         key={2}
         initial={{ x: 1050, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
@@ -26,7 +26,7 @@ const Transition = () => {
         exit={{ x: 1050, opacity: 0 }}
       />
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen"
         key={3}
         initial={{ x: 1050, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/global.css";
-import { useRouter } from "next/router";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import dynamic from "next/dynamic";
 import { Analytics } from "@vercel/analytics/react";
 
@@ -9,15 +8,12 @@ const DynamicLayout = dynamic(() => import("@/components/Layout"), { ssr: false 
 const DynamicTransition = dynamic(() => import("@/components/Transition"), { ssr: false });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
   return (
     <DynamicLayout>
       <AnimatePresence mode="wait" initial={false}>
-        <div key={router.route} className="h-full">
-          <DynamicTransition />
-          <Component {...pageProps} />
-          <Analytics />
-        </div>
+        <DynamicTransition />
+        <Component {...pageProps} />
+        <Analytics />
       </AnimatePresence>
     </DynamicLayout>
   );
