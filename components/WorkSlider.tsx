9f8f3a3f48ef8fe898fbce0f2becdea30c4dde92
Peace Oghenevwefe <Peazzycole@gmail.com>
2024-01-12 22:11:02 +0100

    adjusted slider text

--- components/WorkSlider.tsx
+++ components/WorkSlider.tsx
@@ -111,7 +111,7 @@ const WorkSlider = () => {
                     />
                     <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                     <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                      <div className="flex items-center gap-x-2 text-[13px] xl:text-xl tracking-[0.2em] text-white hover:text-black">
+                      <div className="flex items-center gap-x-2 text-[8px] xl:text-xl tracking-[0.2em] text-white hover:text-black">
                         <div className="delay-100">LIVE</div>
                         <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
                           PROJECT
